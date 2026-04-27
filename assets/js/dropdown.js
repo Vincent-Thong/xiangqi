67ac00e1c70a118e6b40109d1a5bbd93731c545a
V <77708896+Vincent-Thong@users.noreply.github.com>
2026-04-27 17:08:45 +0800

    Implement dropdown toggle and close functionality
    Added functionality to toggle and close dropdown on click events.

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -13,3 +13,22 @@ window.onclick = function(e) {
     }
   }
 }
+
+/* When the user clicks on the button, toggle between hiding and showing the dropdown content */
+function myInvestmentFunction() {
+  document.getElementById("myInvestmentDropdown").classList.toggle("show");
+}
+
+/* Close the dropdown if the user clicks outside of it */
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
